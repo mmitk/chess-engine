0e33a0a497b40a6ffd965689c60428cafc1dd37b
mmitk <51462992+mmitk@users.noreply.github.com>
2020-03-05 20:26:28 -0500

    updated svm class

--- src/models.py
+++ src/models.py
@@ -1,10 +1,26 @@
 from sklearn import svm
+import pickle
 
 class svm:
     
-    def __init__(self, ):
-        self._model = svm.SVC(kernel = 'rbf')
+    def __init__(self, filename = None):
+        if not filename:
+            self._model = svm.SVC(kernel = 'rbf')
     
-    def fit(dataset):
-        return
+    def fit(self, dataset):
+        self._model.fit(dataset)
     
+    def predict(self, dataset):
+        return self._model.predict(dataset)
+
+    def predict_proba(self, dataset):
+        return self._model.predict_proba(dataset)
+    
+    def write_file(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self._model, file)
+    
+    def load_file(self, filename):
+        with open(filename, 'rb') as file:
+            self._model = pickle.load(file)
+
