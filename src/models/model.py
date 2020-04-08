49fcd46278a4c5be4b8f2f05d9fa0d2f6742415f
mmitk <51462992+mmitk@users.noreply.github.com>
2020-04-07 22:04:51 -0400

    redo of model.py, new SVM model (classifier), preproccer class added

--- src/models/model.py
+++ src/models/model.py
@@ -9,11 +9,11 @@ import datetime
 import time
 import os
 import util
-class svm_eval():
+class svm():
     
-    def __init__(self, filename = None, historic = False):
+    def __init__(self, filename = None, historic = False, gam = 1/8):
         if filename is None:
-                self._model = svm.SVR(kernel = 'rbf', gamma='auto')
+                self._model = svm.SVM(kernel = 'rbf', gamma=gam)
 
 
         elif filename is not None and historic == True:
@@ -23,57 +23,29 @@ class svm_eval():
                     self._model = pickle.load(file)
             except EnvironmentError as e:
                 print(e)
-                self._model = svm.SVR(kernel = 'rbf', gamma='auto')
+                
                 
         # placeholder!!!
         else: 
-            self._model = svm.SVR(kernel = 'rbf')
+            self._model = svm.SVR(kernel = 'rbf', gamma=gam)
       
     
     def fit(self, dataset, formatted = True):
-        if formatted and dataset is not None:
-            input_data = {}
-            target_data = {}
-            i = 0
-            for row in dataset:
-                input_data[i] = row['input']
-                target_data[i] = row['target']
-                i+=1
-            input_df = pd.DataFrame.from_dict(input_data, orient='index')
-            output_df = pd.DataFrame.from_dict(target_data, orient='index')
-            self._model.fit(input_df, output_df)
-            self.log('Model (Re)TRAINED')
-
-            '''
-        if formatted == False:
-            df = pd.DataFrame(dataset)
-            for i in range(df.input):
-                df.input[i] = np.asarray(list(df.input[i].encode('utf8')))
-            self._model.fit(df)
-            '''
-
+        y = dataset.pop('class')
+        self._model.fit(dataset,y)
     
-    def predict(self, dataset, formatted = True):
-        if formatted==False and dataset is not None:
-            #df = pd.DataFrame(dataset)
-            #for inp in df.input:
-                #inp = list(inp.encode('utf8'))
-            try:
-                pred = self._model.predict(np.asarray(list(dataset.encode('utf8'))).reshape(-1, 1))
-                self.log('Model PREDICT')
-            except Exception as e:
-                #print('EVAL VALUE:',eval.evaluate_board(chess.Board(dataset.input[0].decode('utf8'))))
-                df = pd.DataFrame(np.asarray(list(dataset.encode('utf8'))))
-                df['target'] = float(eval.evaluate_board(chess.Board(dataset)))
-                target = df.pop('target')
-                self._model.fit(df, target)
-                self.log('Model TRAINED and PREDICT')
-                return self._model.predict(np.asarray(list(dataset.encode('utf8'))).reshape(-1, 1))
+    def predict(self, dataset):
+        try:
+            self.model.predict
+        except Exception as e:
+            return False
+
 
     def predict_proba(self, dataset, formatted = True):
-         if formatted and dataset is not None:
-            target = dataset.pop('target')
+        try:
             return self._model.predict_proba(dataset)
+        except Exception:
+            return 1
     
     def write_file(self, filename):
         with open(filename, 'wb') as file:
@@ -86,3 +58,103 @@ class svm_eval():
 
     def log(self, message):
         util.log(message,logger_str="svm_eval", debug_level=1)
+
+
+
+
+
+class preproccessor(object):
+
+    def __init__(self):
+        super.__init__()
+
+    def fit(self, filename = None, raw_data = None):
+        if filename is not None:
+            self.raw_data = pd.read_csv()
+        elif raw_data is not None:
+            self.raw_data = pd.DataFrame(raw_data)
+        
+
+    def transform(self):
+        data = self.raw_data
+        data['state'] = data['state'].apply(self.serialize2)
+        data = pd.concat([data['state'].apply(pd.Series), data['move']], axis = 1)
+        data = data.rename(columns = {0:'b1',1:'b2',2:'b3',3:'b4',4:'b5'})
+        data['b1'] = data['b1'].apply(np.log)
+        data['b2'] = data['b2'].apply(np.log)
+        data['b3'] = data['b3'].apply(np.log)
+        data['b4'] = data['b4'].apply(np.log)
+        move = data.pop('move')
+        data = pd.concat([data,move.apply(listify).apply(pd.Series)], axis = 1)
+        data = data.rename(columns = {0:'m1',1:'m2',2:'m3',3:'m4',4:'m5'})
+        data['m1'] = data['m1'].apply(encode_move)
+        data['m2'] = data['m2'].apply(encode_move)
+        data['m3'] = data['m3'].apply(encode_move)
+        data['m4'] = data['m4'].apply(encode_move)
+        return data
+    
+    def encode_move(self,m):
+        return ord(m)
+
+
+    def listify(self,string):
+        return list(string)
+        
+    def bin_matrix_to_decimal(self, matrix):
+        m = matrix.reshape(-1)
+        dec = 0
+        j = len(m)-1
+        for i in range(len(m)-1):
+            if m[i]:
+                dec += 2**j
+            j -= 1
+        return float(dec)
+
+    def serialize(self,board):
+        import numpy as np
+        assert board.is_valid()
+
+        bstate = np.zeros(64, np.uint8)
+        for i in range(64):
+            pp = board.piece_at(i)
+            if pp is not None:
+                #print(i, pp.symbol())
+                bstate[i] = {"P": 1, "N": 2, "B": 3, "R": 4, "Q": 5, "K": 6, \
+                        "p": 9, "n":10, "b":11, "r":12, "q":13, "k": 14}[pp.symbol()]
+        if board.has_queenside_castling_rights(chess.WHITE):
+            assert bstate[0] == 4
+            bstate[0] = 7
+        if board.has_kingside_castling_rights(chess.WHITE):
+            assert bstate[7] == 4
+            bstate[7] = 7
+        if board.has_queenside_castling_rights(chess.BLACK):
+            assert bstate[56] == 8+4
+            bstate[56] = 8+7
+        if board.has_kingside_castling_rights(chess.BLACK):
+            assert bstate[63] == 8+4
+            bstate[63] = 8+7
+
+        if board.ep_square is not None:
+            assert bstate[self.board.ep_square] == 0
+            bstate[self.board.ep_square] = 8
+        bstate = bstate.reshape(8,8)
+
+
+        state = []
+   
+        state.append(bin_matrix_to_decimal((bstate>>3)&1))
+        state.append(bin_matrix_to_decimal((bstate>>2)&1))
+        state.append(bin_matrix_to_decimal((bstate>>1)&1))
+        state.append(bin_matrix_to_decimal((bstate>>0)&1))
+
+        state.append(((board.turn*1.0)+50))
+    
+        return state
+
+    def serialize2(self,board):
+        return self.serialize(chess.Board(str(board)))
+
+
+    
+    
+        
