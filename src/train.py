6780df3afba35b1ad9e7b54f2014e347018f2583
mmitk <51462992+mmitk@users.noreply.github.com>
2020-04-13 16:34:21 -0400

    initial training and gameplay environment initialized

---
+++ src/train.py
@@ -0,0 +1,19 @@
+import util
+import alphabeta as ab 
+import mcts
+from env import chessGame
+import chess 
+from multiprocessing import Pool, Manager, Process
+
+if __name__ == '__main__':
+    manager = Manager()
+    a1 = ab.alphabeta_agent()
+    m = mcts.mcts_agent(manager)
+    a2 = ab.alphabeta_agent()
+    board = chess.Board()
+
+    game = chessGame(a2, a1)
+
+    game.set_board(board)
+    game.play_out()
+
