cdfe1e35a80d1542b6400e1e4c29d0c375f100b0
Auris Kveraga <auris_kveraga@student.uml.edu>
2020-04-14 04:34:43 -0400

    Updates to AB, EVAL etc

--- src/train.py
+++ src/train.py
@@ -6,14 +6,15 @@ import chess
 from multiprocessing import Pool, Manager, Process
 
 if __name__ == '__main__':
+    util.parse_cmd_line()
+
     manager = Manager()
     a1 = ab.alphabeta_agent()
     m = mcts.mcts_agent(manager)
     a2 = ab.alphabeta_agent()
     board = chess.Board()
 
-    game = chessGame(a2, m)
+    game = chessGame(a1, a2)
 
     game.set_board(board)
     game.play_out()
-
